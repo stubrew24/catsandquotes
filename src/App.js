8407d0c08cc63e40ac20b7a13c7dee0a0e4f3c03
Stuart Sewell <hello@stuartsewell.dev>
2020-03-04 15:52:51 +0000

    Master blank

--- src/App.js
+++ src/App.js
@@ -1,28 +1,7 @@
-import React, {useEffect} from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { apiRequest } from './store/actions';
+import React from 'react';
 
 function App() {
-
-  const {catImageUrl, quoteText} = useSelector(state => state)
-  const dispatch = useDispatch()
-
-  const handleClick = () => dispatch(apiRequest())
-
-  useEffect(() => {
-    dispatch(apiRequest())
-  }, [])
-
-  return (
-    <div className="container">
-      <h1>Cats + Quotes</h1>
-      <div className="row">
-        <img src={catImageUrl} alt="A cat"/>
-      </div>
-      <blockquote>{quoteText}</blockquote>
-      <button onClick={handleClick}>Gimme more...</button>
-    </div>
-  );
+  return <div>Hello, world!</div>;
 }
 
 export default App;
--- src/store/actions.js
+++
@@ -1,7 +0,0 @@
-export const API_REQUEST = "API_REQUEST";
-export const API_SUCCESS = "API_SUCCESS";
-export const API_FAILURE = "API_FAILURE";
-
-export const apiRequest = () => ({ type: API_REQUEST });
-export const apiSuccess = payload => ({ type: API_SUCCESS, payload });
-export const apiFailure = error => ({ type: API_FAILURE, error });
